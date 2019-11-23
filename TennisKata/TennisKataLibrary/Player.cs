7cc802ee38a0533981647488e9ac2167e66a74ba
hysmun <throdlemassif@hotmail.com>
2019-11-23 15:29:03 +0100

    Begin project Add Score and player

---
+++ TennisKata/TennisKataLibrary/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TennisKataLibrary
+{
+    public class Player
+    {
+        public Player(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public string Name { get; private set; }
+
+        public string Surname { get; private set; }
+    }
+}
---
+++ TennisKata/TennisKataLibrary/Score.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TennisKataLibrary
+{
+    public class Score
+    {
+        public static Score Love = new Score("love", 0);
+        public static Score Fifteen = new Score("fifteen", 15);
+        public static Score Thirty = new Score("thirty", 30);
+        public static Score Forty = new Score("forty", 40);
+        public static Score Advantage = new Score("advantage", 40);
+
+        private Score(string name, int points)
+        {
+            Name = name;
+            Points = points;
+        }
+
+        public string Name { get; private set; }
+        public int Points { get; private set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+                return false;
+            if(obj is Score s) 
+                return Equals(s);
+            return false;
+        }
+    }
+}
